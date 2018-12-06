a9180e37d8111d360347b363022542cb9ac0c561
yogeshktm <yogeshwaran.sundaramoorthy@photoninfotech.net>
2018-12-06 20:58:29 +0530

    Updates

---
+++ precache-manifest.fa9eb5dcc8460945e570992f150df8ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a69d6614ab32b008890e",
+    "url": "/weather-app-react/static/js/runtime~main.a69d6614.js"
+  },
+  {
+    "revision": "95fc52d45fa21a5cbf59",
+    "url": "/weather-app-react/static/js/main.95fc52d4.chunk.js"
+  },
+  {
+    "revision": "e06e154eb3b0feb85b3d",
+    "url": "/weather-app-react/static/js/1.e06e154e.chunk.js"
+  },
+  {
+    "revision": "95fc52d45fa21a5cbf59",
+    "url": "/weather-app-react/static/css/main.93adc94e.chunk.css"
+  },
+  {
+    "revision": "937d107cfc92039cbb4722ec5666c1c4",
+    "url": "/weather-app-react/index.html"
+  }
+];
